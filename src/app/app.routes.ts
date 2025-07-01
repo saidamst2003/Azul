b20ff10b7a07465e6c70febd7d96aeb85c115513
saidamst2003 <saida2003mst@gmail.com>
2025-07-01 17:58:51 +0100

    j'ai creer des  nav bar et et atelier design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Ateliers } from './ateliers/ateliers';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Ateliers }, 
+];
