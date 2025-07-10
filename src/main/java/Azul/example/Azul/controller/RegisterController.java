6c9d1759c4c0dedc93febe0f6829e9ff600bda66
saidamst2003 <saida2003mst@gmail.com>
2025-07-10 10:02:55 +0100

    j'ai modifierRegisterController

---
+++ src/main/java/Azul/example/Azul/controller/RegisterController.java
@@ -0,0 +1,26 @@
+package Azul.example.Azul.controller;
+
+import Azul.example.Azul.dto.RegisterDTO;
+import jakarta.validation.Valid;
+import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user/register")
+@CrossOrigin
+public class RegisterController {
+
+    private final Authservice authService;
+
+    public RegisterController(final Authservice authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/{role}")
+    public ResponseEntity<User> register(@Valid @RequestBody RegisterDTO registerDTO, @PathVariable String role) {
+        User registeredUser = authService.registerUser(registerDTO, role);
+        return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+    }
+}
