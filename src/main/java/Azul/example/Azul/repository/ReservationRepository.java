be0c7b4b414c444a9a33a6619e9d7837d12698bf
saidamst2003 <saida2003mst@gmail.com>
2025-08-08 15:17:06 +0100

    j'ai creer CreateReservationDTO

--- src/main/java/Azul/example/Azul/repository/ReservationRepository.java
+++ src/main/java/Azul/example/Azul/repository/ReservationRepository.java
@@ -1,4 +1,13 @@
 package Azul.example.Azul.repository;
 
-public class ReservationRepository {
+import Azul.example.Azul.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    Optional<Reservation> findByAtelierIdAndUserId(Long atelierId, Long userId);
+    List<Reservation> findAllByUserId(Long userId);
+    List<Reservation> findAllByAtelierId(Long atelierId);
 }
